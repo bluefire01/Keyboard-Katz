9159feaadca07e32aff9079735b7cef9c7c58f71
Young <bluefire01@server>
2012-10-26 18:12:47 -0400

    DBApi SQLite Version

---
+++ KeybirdKatzDBApi/KeybirdKatzDBApi/DBApi.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SQLite;
+
+namespace KeybirdKatzDBApi
+{
+    public class DBApi
+    {
+        private static readonly DBApi instance = new DBApi();
+
+        static private KeybirdKatzDataSetTableAdapters.PlayerTableAdapter playerTableAdapter;
+        static private KeybirdKatzDataSetTableAdapters.PlayerSummaryTableAdapter playerSummaryTableAdapter;
+        static private KeybirdKatzDataSet kkDataSet;
+
+        public long addPlayer(String playerName)
+        {
+            playerTableAdapter.Fill(kkDataSet.Player);
+            Random random = new Random();
+            long key = random.Next();
+            try
+            {
+                key = random.Next();
+                kkDataSet.Player.AddPlayerRow(key, playerName);
+                playerTableAdapter.Update(kkDataSet);
+                //System.Console.Out.WriteLine("Added Player {0} {1}", key, playerName);
+                return key;
+            }
+            catch (System.Data.ConstraintException c)
+            {
+                System.Console.Out.WriteLine(c.Message);
+                return addPlayer(playerName);
+            }
+            catch (Exception e)
+            {
+                System.Console.Out.WriteLine(e.Message);
+                return -1;
+            }
+        }
+
+        public bool deletePlayer(long playerID)
+        {
+            try
+            {
+                KeybirdKatzDataSet.PlayerRow row = kkDataSet.Player.FindByPlayerID(playerID);
+                //mark row for deletion
+                row.Delete();
+                //commit change
+                playerTableAdapter.Update(kkDataSet);
+                return true;
+            }
+            catch (Exception e)
+            {
+                System.Console.Out.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public void deleteAllPlayers()
+        {
+            int i = 0;
+            while (i < kkDataSet.Player.Rows.Count)
+            {
+                //mark row for deletion
+                kkDataSet.Player.Rows[i].Delete();
+                i++;
+            }
+            //commit changes
+            playerTableAdapter.Update(kkDataSet);
+
+        }
+
+        public void changePlayerName(long playerID, String newName)
+        {
+            KeybirdKatzDataSet.PlayerRow row = kkDataSet.Player.FindByPlayerID(playerID);
+            //System.Console.Out.WriteLine(row[1]);
+            //column 0 is PlayerID, column 1 is PlayerName
+            row.PlayerName = newName;
+            playerTableAdapter.Update(kkDataSet);
+        }
+
+        public String getPlayerName(long playerID)
+        {
+            KeybirdKatzDataSet.PlayerRow row = kkDataSet.Player.FindByPlayerID(playerID);
+            return row.PlayerName;
+        }
+
+        private DBApi()
+        {
+            
+            playerTableAdapter = new KeybirdKatzDataSetTableAdapters.PlayerTableAdapter();
+            playerSummaryTableAdapter = new KeybirdKatzDataSetTableAdapters.PlayerSummaryTableAdapter();
+            kkDataSet = new KeybirdKatzDataSet();
+            playerTableAdapter.Fill(kkDataSet.Player);
+        }
+
+        public static DBApi Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
---
+++ KeybirdKatzDBApi/KeybirdKatzDBApi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+
+namespace KeybirdKatzDBApi
+{
+    class Program
+    {
+        private static DBApi dbApi;
+        private static Random random = new Random(); 
+        List<long> userKeys;
+
+
+        static void Main(string[] args)
+        {
+            dbApi = DBApi.Instance;
+            Program prgm = new Program();
+            prgm.userKeys = new List<long>();
+
+            dbApi.deleteAllPlayers();
+
+            for (int i = 0; i < 10; i++)
+            {
+                long nKey = dbApi.addPlayer(random.Next(10).ToString());
+                System.Console.Out.WriteLine(nKey);
+                prgm.userKeys.Add(nKey);
+            }
+
+            String theName = "Mike";
+            System.Console.Out.WriteLine("Change player name {0} with name {1} to {2}", prgm.userKeys[0], dbApi.getPlayerName(prgm.userKeys[0]), theName);
+            dbApi.changePlayerName(prgm.userKeys[0], theName);
+            
+            System.Console.Out.WriteLine("Delete Key {0}", prgm.userKeys[1]);
+            dbApi.deletePlayer(prgm.userKeys[1]);
+            dbApi.deleteAllPlayers();
+        }
+    }
+}
